70a87d9daf08d36e1d14f030e9d7d06d8ddb241b
wikifrost <159472972+wikifrost@users.noreply.github.com>
2024-04-09 22:15:19 +0100

    Create graph_load_pickle.py

---
+++ graph_load_pickle.py
@@ -0,0 +1,31 @@
+import pickle
+
+# LOAD THE DATA, PICKLE FORMAT
+def load_data_from_pickle(file_path):
+
+    try:
+        with open(file_path, 'rb') as f:
+            data = pickle.load(f)
+        return data
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+# load the data
+pretrained_loss = load_data_from_pickle('pretraining_loss.pkl') 
+
+finetuning_train_loss = load_data_from_pickle('finetuning_train_loss.pkl')
+finetuning_train_acc = load_data_from_pickle('finetuning_train_accuracy.pkl')
+
+finetuning_val_loss = load_data_from_pickle('finetuning_val_loss.pkl')
+finetuning_val_acc = load_data_from_pickle('finetuning_val_accuracy.pkl')
+
+benchmark_train_loss = load_data_from_pickle('benchmark_train_loss.pkl')
+benchmark_train_acc = load_data_from_pickle('benchmark_train_accuracy.pkl')
+
+benchmark_val_loss = load_data_from_pickle('benchmark_val_loss.pkl')
+benchmark_val_acc = load_data_from_pickle('benchmark_val_accuracy.pkl')
+
